2c3ed60beab5b81d235f3a28db7d97e57fea84f4
backpackboyys <backpackboyys@gmail.com>
2026-08-27 14:09:12 -0400

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
